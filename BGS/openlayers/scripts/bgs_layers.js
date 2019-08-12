07827fefc2a74ee390f27fc108377c0d94cc78d1
andreas.mitrou@geovation.uk <andreas.mitrou@geovation.uk>
2019-08-12 14:34:38 +0100

    Open Lyaers Implementation

---
+++ BGS/openlayers/scripts/bgs_layers.js
@@ -0,0 +1,8 @@
+var bgs_wms_services = [{
+    wms_url:"http://ogc.bgs.ac.uk/cgi-bin/BGS_Bedrock_and_Superficial_Geology/wms",
+    layers:[
+                {name:"BGS_EN_Bedrock_and_Superficial_Geology"},
+                {name:"GBR_BGS_625k_BLT"},
+                {name:"GBR_BGS_625k_BLS"}
+            ]
+}]
